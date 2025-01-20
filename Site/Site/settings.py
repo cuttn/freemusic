42e11949a3a76391fd8ca796d06da0a5b21cb6d1
cuttn <curtis@jelve.com>
2025-01-20 17:05:25 -0500

    aksdfjasdfasdf

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2yee^flf$it+v#pbeyhgvfw%#5!#__q4*&8url1j29om=!m&6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["free-music.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["free-music.onrender.com", "127.0.0.1", "0.0.0.0"]
 
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
 
